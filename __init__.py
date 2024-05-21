676c6bb607717096c5e1b39b6e31779ccc4ba630
cupcake-mastur <137142248+cupcake-mastur@users.noreply.github.com>
2024-05-21 22:14:56 +0800

    add test sign up page

--- __init__.py
+++ __init__.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
+
+
 @app.route('/')
 def home():
     return 'Hello world!'
+
+
+@app.route('/sign_up')
+def sign_up():
+    return render_template("customer/sign_up.html")
+
 
 if __name__ == '__main__':
     app.run()
