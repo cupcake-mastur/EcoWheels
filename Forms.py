3df02037b9dbe9a6b60a5916a1d7a240766d3701
cupcake-mastur <137142248+cupcake-mastur@users.noreply.github.com>
2024-05-29 22:34:30 +0800

    adding .env file

--- Forms.py
+++ Forms.py
@@ -1,16 +1,11 @@
 from wtforms import Form, EmailField, validators, PasswordField, StringField, IntegerField
 
 
-def no_special_characters(Form, field):
-    special_characters = "!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
-    if any(char in special_characters for char in field.data):
-        raise validators.ValidationError("Special characters are not allowed.")
-
-
 class CreateUserForm(Form):
     email = EmailField('Email', [validators.DataRequired(), validators.Email()])
-    full_name = StringField('Full Name', validators=[validators.DataRequired(), no_special_characters])
-    username = StringField('Username', validators=[validators.DataRequired(), no_special_characters])
-    phone_number = IntegerField('Phone Number', [validators.DataRequired(), validators.NumberRange(min=00000000, max=99999999)])
+    full_name = StringField('Full Name', validators=[validators.DataRequired()])
+    username = StringField('Username', validators=[validators.DataRequired()])
+    phone_number = IntegerField('Phone Number', [validators.DataRequired(),
+                                                 validators.NumberRange(min=00000000, max=99999999)])
     password = PasswordField('Password', [validators.DataRequired(), validators.length(min=8, max=30)])
     confirm_password = PasswordField('Confirm Password', [validators.DataRequired(), validators.length(min=8, max=30)])
--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from Forms import CreateUserForm
-from werkzeug.security import generate_password_hash
 import hashlib
+from dotenv import load_dotenv, find_dotenv
+import os
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exists
 import mysql.connector
 
 app = Flask(__name__)
 db = SQLAlchemy()
+load_dotenv(find_dotenv())
 db_2 = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -15,31 +17,13 @@ db_2 = mysql.connector.connect(
     database="eco_wheels"
 )
 
-def create_app():
-    app = Flask(__name__)
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:EcoWheels123@127.0.0.1:3306/eco_wheels"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = "mysecret"
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://JY:123456@127.0.0.1:3306/ASPJ"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SECRET_KEY"] = "mysecret"
-
-
-    db.init_app(app)
-
-    with app.app_context():
-
-        db.create_all()  # Create sql tables if they don't already exist
-
-    return app
-app = create_app()
 # def create_app():
 #     app = Flask(__name__)
-#     app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://JY:123456@127.0.0.1:3306/ASPJ"
-#     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-#     app.config["SECRET_KEY"] = "mysecret"
+#
+#     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI2")
+#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#     app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+#
 #
 #     db.init_app(app)
 #
@@ -48,18 +32,13 @@ app = create_app()
 #         db.create_all()  # Create sql tables
 #
 #     return app
-
-        import model
-        db.create_all()  # Create sql tables
-
-    return app
+# app = create_app()
 
 
 #JIAYINGG the following 4 lines of commented code is ursss! 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://JY:123456@127.0.0.1:3306/ASPJ"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# SQL here
-app.config["SECRET_KEY"] = "mysecret"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 db.init_app(app)
 
@@ -90,7 +69,6 @@ def sign_up():
         email = create_user_form.email.data
         phone_number = create_user_form.phone_number.data
         password = create_user_form.password.data
-        password_hash = generate_password_hash(password)
         password_bytes = password.encode('utf-8')
         hashed_password = hashlib.sha256(password_bytes).hexdigest()
         confirm_password = create_user_form.confirm_password.data
@@ -101,8 +79,6 @@ def sign_up():
         phone_number_exists = db.session.query(exists().where(model.User.phone_number == phone_number)).scalar()
 
         if user_exists:
-            error = "User already exists!"
-        # Check if the passwords match
             error = "Username already exists!"
         elif email_exists:
             error = "Email is already registered!"
@@ -110,9 +86,15 @@ def sign_up():
             error = "Phone number is already registered!"
         elif password != confirm_password:
             error = "Passwords do not match!"
+        elif len(str(phone_number)) != 8:
+            error = "Phone number must be 8 digits."
         else:
+            # Validate special characters
+            if any(char in "!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?'" for char in full_name):
+                error = "Special characters are not allowed in the full name."
+
+        if error is None:
             # Create a new user
-            new_user = model.User(full_name=full_name, username=username, email=email, phone_number=phone_number, password_hash=password_hash)
             new_user = model.User(full_name=full_name, username=username, email=email, phone_number=phone_number, password_hash=hashed_password)
             db.session.add(new_user)
             db.session.commit()
@@ -143,78 +125,77 @@ def confirmation():
     return "Thank you for your order!"
 
 
-
-@app.route('/process_payment', methods=['POST'])
-def process_payment():
-    try:
-        fullname = request.form['firstname']
-        email = request.form['email']
-        address = request.form['address']
-        city = request.form['city']
-        state = request.form['state']
-        zip_code = request.form['zip']
-        card_name = request.form['cardname']
-        card_number = request.form['cardnumber']
-        exp_month = request.form['expmonth']
-        exp_year = request.form['expyear']
-        cvv = request.form['cvv']
-        cursor = db_2.cursor()
-        query = "INSERT INTO orders (fullname, email, address, city, state, zip_code, card_name, card_number, exp_month, exp_year, cvv) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        values = (fullname, email, address, city, state, zip_code, card_name, card_number, exp_month, exp_year, cvv)
-        cursor.execute(query, values)
-        db_2.commit()
-    except Exception as e:
-        db_2.session.rollback()
-        print("Failed to process payment:", e)  # Log the error or use a logging framework
-        return "Error processing payment", 500
-    return redirect(url_for('confirmation'))
-
-@app.route('/view_payment')
-def view_payment():
-    cursor = db_2.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM orders")
-    orders = cursor.fetchall()
-    cursor.close()
-    return render_template("admin/view_payment.html", orders=orders)
-
-
-@app.route('/update_payment/<int:id>', methods=['GET', 'POST'])
-def update_payment(id):
-    if request.method == 'POST':
-        fullname = request.form['fullname']
-        email = request.form['email']
-        address = request.form['address']
-        city = request.form['city']
-        state = request.form['state']
-        zip_code = request.form['zip']
-        card_name = request.form['cardname']
-        card_number = request.form['cardnumber']
-        exp_month = request.form['expmonth']
-        exp_year = request.form['expyear']
-        cvv = request.form['cvv']
-
-        cursor = db_2.cursor()
-        cursor.execute("""
-            UPDATE orders SET fullname=%s, email=%s, address=%s, city=%s, state=%s, zip_code=%s, card_name=%s, card_number=%s, exp_month=%s, exp_year=%s, cvv=%s
-            WHERE order_id=%s
-        """, (fullname, email, address, city, state, zip_code, card_name, card_number, exp_month, exp_year, cvv, id))
-        db_2.commit()
-        cursor.close()
-        return redirect(url_for('view_payment'))
-    else:
-        cursor = db_2.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM orders WHERE order_id = %s", (id,))
-        order = cursor.fetchone()
-        cursor.close()
-        return render_template('admin/update_payment.html', order=order)
-
-@app.route('/delete_payment/<int:id>', methods=['POST'])
-def delete_payment(id):
-    cursor = db_2.cursor()
-    cursor.execute("DELETE FROM orders WHERE order_id = %s", (id,))
-    db_2.commit()
-    cursor.close()
-    return redirect(url_for('view_payment'))
+# @app.route('/process_payment', methods=['POST'])
+# def process_payment():
+#     try:
+#         fullname = request.form['firstname']
+#         email = request.form['email']
+#         address = request.form['address']
+#         city = request.form['city']
+#         state = request.form['state']
+#         zip_code = request.form['zip']
+#         card_name = request.form['cardname']
+#         card_number = request.form['cardnumber']
+#         exp_month = request.form['expmonth']
+#         exp_year = request.form['expyear']
+#         cvv = request.form['cvv']
+#         cursor = db_2.cursor()
+#         query = "INSERT INTO orders (fullname, email, address, city, state, zip_code, card_name, card_number, exp_month, exp_year, cvv) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+#         values = (fullname, email, address, city, state, zip_code, card_name, card_number, exp_month, exp_year, cvv)
+#         cursor.execute(query, values)
+#         db_2.commit()
+#     except Exception as e:
+#         db_2.session.rollback()
+#         print("Failed to process payment:", e)  # Log the error or use a logging framework
+#         return "Error processing payment", 500
+#     return redirect(url_for('confirmation'))
+#
+# @app.route('/view_payment')
+# def view_payment():
+#     cursor = db_2.cursor(dictionary=True)
+#     cursor.execute("SELECT * FROM orders")
+#     orders = cursor.fetchall()
+#     cursor.close()
+#     return render_template("admin/view_payment.html", orders=orders)
+#
+#
+# @app.route('/update_payment/<int:id>', methods=['GET', 'POST'])
+# def update_payment(id):
+#     if request.method == 'POST':
+#         fullname = request.form['fullname']
+#         email = request.form['email']
+#         address = request.form['address']
+#         city = request.form['city']
+#         state = request.form['state']
+#         zip_code = request.form['zip']
+#         card_name = request.form['cardname']
+#         card_number = request.form['cardnumber']
+#         exp_month = request.form['expmonth']
+#         exp_year = request.form['expyear']
+#         cvv = request.form['cvv']
+#
+#         cursor = db_2.cursor()
+#         cursor.execute("""
+#             UPDATE orders SET fullname=%s, email=%s, address=%s, city=%s, state=%s, zip_code=%s, card_name=%s, card_number=%s, exp_month=%s, exp_year=%s, cvv=%s
+#             WHERE order_id=%s
+#         """, (fullname, email, address, city, state, zip_code, card_name, card_number, exp_month, exp_year, cvv, id))
+#         db_2.commit()
+#         cursor.close()
+#         return redirect(url_for('view_payment'))
+#     else:
+#         cursor = db_2.cursor(dictionary=True)
+#         cursor.execute("SELECT * FROM orders WHERE order_id = %s", (id,))
+#         order = cursor.fetchone()
+#         cursor.close()
+#         return render_template('admin/update_payment.html', order=order)
+#
+# @app.route('/delete_payment/<int:id>', methods=['POST'])
+# def delete_payment(id):
+#     cursor = db_2.cursor()
+#     cursor.execute("DELETE FROM orders WHERE order_id = %s", (id,))
+#     db_2.commit()
+#     cursor.close()
+#     return redirect(url_for('view_payment'))
 
 
 # NEED TO METHOD = 'POST' THESE ADMIN PAGES
