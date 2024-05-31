76f66140b72357bfd005b4bb48b8127cefd80971
cupcake-mastur <137142248+cupcake-mastur@users.noreply.github.com>
2024-05-31 20:41:00 +0800

    adding login codes (got error)

--- Forms.py
+++ Forms.py
@@ -9,3 +9,8 @@ class CreateUserForm(Form):
                                                  validators.NumberRange(min=00000000, max=99999999)])
     password = PasswordField('Password', [validators.DataRequired(), validators.length(min=8, max=30)])
     confirm_password = PasswordField('Confirm Password', [validators.DataRequired(), validators.length(min=8, max=30)])
+
+
+class LoginForm(Form):
+    email = EmailField('Email', [validators.DataRequired(), validators.Email()])
+    password = PasswordField('Password', [validators.DataRequired(), validators.length(min=8, max=30)])
