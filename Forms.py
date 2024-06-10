dd65ef8277d4736bb8df00632d4915ecdd689632
FancyFar <shaniawsh@gmail.com>
2024-06-10 15:44:08 +0800

    Change forms to wtforms

--- Forms.py
+++ Forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
 from wtforms import Form, EmailField, validators, PasswordField, StringField, IntegerField
-from wtforms.fields.simple import SubmitField
-from wtforms.validators import DataRequired, Length, Regexp
+from wtforms.fields.simple import SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, Regexp, NumberRange
 
 
 class CreateUserForm(Form):
@@ -36,3 +37,25 @@ class AdminLoginForm(FlaskForm):
         )
     ])
     submit = SubmitField('Continue')
+
+
+class CreateVehicleForm(FlaskForm):
+    brand = StringField('Brand', validators=[
+        DataRequired(message="Brand is required."),
+        Regexp(r'^[A-Za-z\s]+$', message="Brand can only contain alphabetic characters and spaces.")
+    ])
+    model = StringField('Model', validators=[
+        DataRequired(message="Model is required."),
+        Length(min=2, max=30, message="Model must be between 2 and 30 characters."),
+        Regexp(r'^[A-Za-z0-9\s]+$', message="Model can only contain alphanumeric characters and spaces.")
+    ])
+    price = IntegerField('Selling Price in SGD ($)', validators=[
+        DataRequired(message="Price is required."),
+        NumberRange(min=0, message="Price must be a positive number.")
+    ])
+    file = FileField('Image', validators=[DataRequired(message="Image is required.")])
+
+    description = TextAreaField('Description', validators=[
+        DataRequired(message="Description is required."),
+        Length(min=30, max=500, message="Description must be between 30 and 500 characters.")
+    ])
