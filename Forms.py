06154d064b8dc3e92628f20671e352f8ffaa8801
FancyFar <shaniawsh@gmail.com>
2024-06-10 14:19:56 +0800

    Update Forms and admin log in

--- Forms.py
+++ Forms.py
@@ -1,4 +1,7 @@
+from flask_wtf import FlaskForm
 from wtforms import Form, EmailField, validators, PasswordField, StringField, IntegerField
+from wtforms.fields.simple import SubmitField
+from wtforms.validators import DataRequired, Length, Regexp
 
 
 class CreateUserForm(Form):
@@ -14,3 +17,22 @@ class CreateUserForm(Form):
 class LoginForm(Form):
     email = EmailField('Email', [validators.DataRequired(), validators.Email()])
     password = PasswordField('Password', [validators.DataRequired(), validators.length(min=8, max=30)])
+
+
+class AdminLoginForm(FlaskForm):
+    class Meta:
+        csrf = True
+
+    username = StringField('Username', validators=[
+        DataRequired(message="Username is required."),
+        Length(min=8, max=30, message="Username must be between 8 and 30 characters.")
+    ])
+    password = PasswordField('Password', validators=[
+        DataRequired(message="Password is required."),
+        Regexp(
+            r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*['\x22]).{8,}$",
+            message=("Password must contain at least 8 characters, including at least one number, "
+                     "one uppercase letter, one lowercase letter, and cannot contain ' or \" characters.")
+        )
+    ])
+    submit = SubmitField('Continue')
