0b7e9b3c5c12cd2ec0b7ce1571fd47f00455289a
FancyFar <shaniawsh@gmail.com>
2024-07-07 12:47:50 +0800

    Update model.py

--- model.py
+++ model.py
@@ -44,9 +44,9 @@ class Admin(db.Model):
     username = db.Column(db.String(64), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
 
-    def set_password(self, password):
-        # Update existing passwords using werkzeug.security
-        self.password_hash = generate_password_hash(password)
+    # def set_password(self, password):
+    #     # Update existing passwords using werkzeug.security
+    #     self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
