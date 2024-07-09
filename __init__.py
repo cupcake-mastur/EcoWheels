c07ee5e5205b2dca469966f949c6278163717b49
Terron <terronlohanderson@gmail.com>
2024-07-09 23:30:30 +0800

    Homepage done-ish
    Homepage done for now

--- __init__.py
+++ __init__.py
@@ -29,6 +29,7 @@ app = Flask(__name__)
 logging.basicConfig(filename='app.log', level=logging.DEBUG,
                     format=f'%(asctime)s %(levelname)s: %(message)s')
 
+
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
