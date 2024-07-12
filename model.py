9bf48fcbdcf1bd019f3f7e03dc32f5274c1096fb
Cheryl Low <125752772+ShareRoll@users.noreply.github.com>
2024-07-12 11:47:26 +0800

    Removing workbench db (converting to stripe)

--- model.py
+++ model.py
@@ -28,23 +28,6 @@ class User(db.Model):
     cvv = db.Column(db.String(3))
 
 
-class Order(db.Model):
-    __tablename__ = 'orders'
-
-    order_id = db.Column(db.Integer, primary_key=True)
-    fullname = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), nullable=False)
-    address = db.Column(db.String(255), nullable=False)
-    city = db.Column(db.String(100), nullable=False)
-    state = db.Column(db.String(100), nullable=False)
-    zip_code = db.Column(db.String(20), nullable=False)
-    card_name = db.Column(db.String(255), nullable=False)
-    card_number = db.Column(db.String(20), nullable=False)
-    exp_month = db.Column(db.String(20), nullable=False)
-    exp_year = db.Column(db.String(4), nullable=False)
-    cvv = db.Column(db.String(5), nullable=False)
-
-
 class Admin(db.Model):
     __tablename__ = 'admins'
     id = db.Column(db.Integer, primary_key=True)
