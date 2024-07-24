b633def585fcd89c6358b59fa3a9f6c22363ff1a
cupcake-mastur <137142248+cupcake-mastur@users.noreply.github.com>
2024-07-24 11:12:13 +0800

    Add error message for sign_up + login

--- Forms.py
+++ Forms.py
@@ -29,16 +29,22 @@ class CreateUserForm(Form):
     full_name = StringField('Full Name', validators=[validators.DataRequired()])
     username = StringField('Username', validators=[validators.DataRequired()])
     phone_number = IntegerField('Phone Number', [validators.DataRequired(), validators.NumberRange(min=00000000, max=99999999)])
-    password = PasswordField('Password', [validators.DataRequired(), validators.length(min=8, max=30), 
-                                          validators.Regexp(regex=re.compile(r'^(?=.*[!@#$%^&*(),.?":{}|<>])'))])
-    confirm_password = PasswordField('Confirm Password', [validators.DataRequired(), validators.length(min=8, max=30), 
-                                          validators.Regexp(regex=re.compile(r'^(?=.*[!@#$%^&*(),.?":{}|<>])'))])
+    password = PasswordField('Password', [validators.DataRequired(), validators.length(min=8, max=30,
+                                            message="New password must be between 8 and 30 characters long."), 
+                                          validators.Regexp(regex=re.compile(r'^(?=.*[!@#$%^&*(),.?":{}|<>])'), 
+                                                            message= "New password must contain at least one special character.")])
+    confirm_password = PasswordField('Confirm Password', [validators.DataRequired(), validators.length(min=8, max=30,
+                                            message="New password must be between 8 and 30 characters long."), 
+                                          validators.Regexp(regex=re.compile(r'^(?=.*[!@#$%^&*(),.?":{}|<>])'), 
+                                                            message= "New password must contain at least one special character.")])
 
 
 class LoginForm(Form):
     email = EmailField('Email', [validators.DataRequired(), validators.Email(), validators.Length(max=50)])
-    password = PasswordField('Password', [validators.DataRequired(), validators.length(min=8, max=30), 
-                                          validators.Regexp(regex=re.compile(r'^(?=.*[!@#$%^&*(),.?":{}|<>])'))])
+    password = PasswordField('Password', [validators.DataRequired(), validators.length(min=8, max=30,
+                                            message="New password must be between 8 and 30 characters long."), 
+                                          validators.Regexp(regex=re.compile(r'^(?=.*[!@#$%^&*(),.?":{}|<>])'), 
+                                                            message= "New password must contain at least one special character.")])
 
 
 class UpdateProfileForm(Form):
